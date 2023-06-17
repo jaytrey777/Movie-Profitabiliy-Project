968a5e88c669721a7680240daa1fe916babdf3f9
Jason R. Tracey <jasonrtracey@gmail.com>
2023-06-17 18:58:28 -0400

    Update Movie_Profitability.sql

--- Movie_Profitability.sql
+++ Movie_Profitability.sql
@@ -26,4 +26,4 @@ USE `Movie Profitability`;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-08 20:16:57
+-- Dump completed on 2023-06-17 18:58:09
